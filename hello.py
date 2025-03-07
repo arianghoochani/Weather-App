91c8703154a5a59da05c90be6fcbbeae45a16a38
arianghoochani <arian_gh22@yahoo.com>
2025-03-07 16:42:44 +0100

    extending the endpoints

--- hello.py
+++ hello.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello_world")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return  "<p>this is the welcome(hello) page!</p>"
+
+@app.route("/index")
+def index():
+    return  "<p>this is the index page!</p>"
+
+@app.route("/info")
+def info():
+    return  "<p>this is the info page!</p>"
