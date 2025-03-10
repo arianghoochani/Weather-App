9b0d0652b14c479258aed9b9f6b54547c2805a59
arianghoochani <arian_gh22@yahoo.com>
2025-03-10 22:01:49 +0100

    a

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,31 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    
-  <div>
-    <h1>
-    hello to my react app
-    </h1>
+    <div>
+      {/* Include Navbar at the top */}
+      <NavBar />
+      <h1>Welcome to the Weather Information Service</h1>
     </div>
-  
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
