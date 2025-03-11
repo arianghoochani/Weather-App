76851efd93cf147949a96897d9575349bf809068
arianghoochani <arian_gh22@yahoo.com>
2025-03-11 22:17:52 +0100

    adding create weather navigate

--- weather_frontend/src/components/Navbar.js
+++ weather_frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React , { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppNavBar } from "baseui/app-nav-bar";
 
 export default function Navbar() {
   const [activeItem, setActiveItem] = useState("Home");
+  const navigate = useNavigate();
   const mainItems = [
     { label: "Home" },
     { label: "Search Weather Info" },
@@ -16,7 +18,17 @@ export default function Navbar() {
 
       title="Weather Information"
       mainItems={mainItems}
-      onMainItemSelect={(item) => setActiveItem(item.label)}
+      // onMainItemSelect={(item) => setActiveItem(item.label)}
+      // onMainItemSelect={(item) => {
+        onMainItemSelect={(item) => {
+          setActiveItem(item.label);
+          if (item.label === "Home") {
+            navigate("/"); 
+          }
+          else if (item.label === "Add Weather Info") {
+            navigate("/add-weather"); 
+          }
+        }}
     />
   );
 }
