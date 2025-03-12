47abaf2b1ba3c81facf1eaf0efa1a701bf126198
arianghoochani <arian_gh22@yahoo.com>
2025-03-12 02:14:19 +0100

    update cors

--- weatherapp_back2/weatherInfoService/app.py
+++ weatherapp_back2/weatherInfoService/app.py
@@ -5,7 +5,9 @@ import config
 from models import Weather
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
-CORS(app.app)
+# CORS(app.app)
+CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+
 migrate = Migrate(app.app, config.db)
 @app.route("/")
 def home():
