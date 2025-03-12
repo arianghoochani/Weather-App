6132585ee945e7f27f9e2d1eaddc21e633614d51
arianghoochani <arian_gh22@yahoo.com>
2025-03-12 09:57:36 +0100

    update headers

--- weatherapp_back2/weatherInfoService/app.py
+++ weatherapp_back2/weatherInfoService/app.py
@@ -7,7 +7,7 @@ from models import Weather
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 # CORS(app.app)
-CORS(app, resources={r"api": {"origins": "http://116.203.184.212:3000/"}},supports_credentials=True, headers=['Content-Type', 'Authorization'])
+CORS(app.app, resources={r"api": {"origins": "http://116.203.184.212:3000/"}},supports_credentials=True, headers=['Content-Type', 'Authorization'])
 # CORS(app.app, resources={r"/api/*": {"origins": ["*","http://116.203.184.212:3000/"]}}, supports_credentials=True)
 
 migrate = Migrate(app.app, config.db)
