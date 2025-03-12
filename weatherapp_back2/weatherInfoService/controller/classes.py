cb60e022bff90d674aa52879f88f2800ac926a67
arianghoochani <arian_gh22@yahoo.com>
2025-03-12 01:00:36 +0100

    a

--- weatherapp_back2/weatherInfoService/controller/classes.py
+++ weatherapp_back2/weatherInfoService/controller/classes.py
@@ -5,7 +5,7 @@ class GetAllWeatherInfoResponse:
     def to_dict(self):
         return {"status": self.status, "weathers": self.weathers}
 
-class createWeatherInfoResponse:
+class CreateWeatherInfoResponse:
     def __init__(self, status, new_weather):
         self.status = status
         self.new_weather = new_weather
