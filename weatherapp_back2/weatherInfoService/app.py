4d6bd3077e7b41a6dddbe2bc6d29a0725ad5c2a0
arianghoochani <arian_gh22@yahoo.com>
2025-03-12 10:49:19 +0100

    update headers

--- weatherapp_back2/weatherInfoService/app.py
+++ weatherapp_back2/weatherInfoService/app.py
@@ -20,21 +20,21 @@ def home():
 
 @app.app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Origin"] = "http://116.203.184.212:3000"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     return response
 
-@app.app.before_request
-def handle_preflight():
-    if request.method == "OPTIONS":
-        response = jsonify({"message": "Preflight OK"})
-        response.headers.add("Access-Control-Allow-Origin", "http://116.203.184.212:3000")
-        response.headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        response.headers.add("Access-Control-Allow-Credentials", "true")
-        return response, 200
+# @app.app.before_request
+# def handle_preflight():
+#     if request.method == "OPTIONS":
+#         response = jsonify({"message": "Preflight OK"})
+#         response.headers.add("Access-Control-Allow-Origin", "http://116.203.184.212:3000")
+#         response.headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+#         response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+#         response.headers.add("Access-Control-Allow-Credentials", "true")
+#         return response, 200
 
 if __name__ == "__main__":
     # uvicorn.run("config:connex_app", host="0.0.0.0", port=8000, reload=True)
