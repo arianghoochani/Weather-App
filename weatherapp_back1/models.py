4d47a04f91115769d266ea76a1cbb1286ab0a0d1
arianghoochani <arian_gh22@yahoo.com>
2025-03-13 23:43:40 +0100

    restructuring the files

--- weatherapp_back1/models.py
+++
@@ -1,7 +0,0 @@
-from db import db  # ✅ Import the existing `db` instance
-
-class Weather(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(50), nullable=False)
-    temperature = db.Column(db.Float, nullable=False)
-    humidity = db.Column(db.Float, nullable=False)
