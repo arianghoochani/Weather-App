bc416b2de8d6e6b16980089f0cb100d670fe57fa
arianghoochani <arian_gh22@yahoo.com>
2025-03-14 00:31:05 +0100

    ad the files cors

--- weatherapp_project/weatherInfoService/app.py
+++ weatherapp_project/weatherInfoService/app.py
@@ -10,7 +10,7 @@ app.add_api(config.basedir / "swagger.yml")
 # CORS(app.app)
 # CORS(app.app, resources={r"/api/*": {"origins": ["*","http://116.203.184.212:3000/"]}}, supports_credentials=True)
 # CORS(app.app, resources={r"/api/*": {"origins": "http://116.203.184.212:3000/"}}, supports_credentials=True)
-CORS(app.app, resources={r"/api/*": {"origins": "http://116.203.184.212:3000"}}, 
+CORS(app.app, resources={r"/api/*": {"origins": "*"}}, 
      supports_credentials=True, allow_headers=["Content-Type", "Authorization"], 
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 migrate = Migrate(app.app, config.db)
@@ -20,7 +20,7 @@ def home():
 
 @app.app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://116.203.184.212:3000"
+    response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Credentials"] = "true"
