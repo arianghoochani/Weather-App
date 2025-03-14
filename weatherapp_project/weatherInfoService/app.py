c391dee983a6e3547dfd7690a8f51a68e04e83a8
arianghoochani <arian_gh22@yahoo.com>
2025-03-14 01:42:16 +0100

    replace swagger

--- weatherapp_project/weatherInfoService/app.py
+++ weatherapp_project/weatherInfoService/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS 
 import data.config as config
 from data.models import Weather
+
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 
@@ -22,17 +23,7 @@ def add_cors_headers(response):
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
-
-# @app.app.before_request
-# def handle_preflight():
-#     if request.method == "OPTIONS":
-#         response = jsonify({"message": "Preflight OK"})
-#         response.headers.add("Access-Control-Allow-Origin", "http://116.203.184.212:3000")
-#         response.headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-#         response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
-#         response.headers.add("Access-Control-Allow-Credentials", "true")
-#         return response, 200
+    return response, 200
 
 if __name__ == "__main__":
     # uvicorn.run("config:connex_app", host="0.0.0.0", port=8000, reload=True)
